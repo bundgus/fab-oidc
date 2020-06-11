1dfb738797836fb311ed7a2fc748443897f23b6b
Mark Bundgus <mark.bundgus@sabre.com>
2020-06-11 13:16:30 -0500

    added forwarding based on login next= parameter to redirect to the original url requested before login redirect

--- fab_oidc/views.py
+++ fab_oidc/views.py
@@ -27,29 +27,45 @@ class AuthOIDCView(AuthOIDView):
         @self.appbuilder.sm.oid.require_login
         def handle_login():
             log.debug(f"oidc.user_getfield('email') {oidc.user_getfield('email')}")
-            log.debug(f'request.args["next"], {request.args["next"]}')
+            # log.debug(f'request.args["next"], {request.args["next"]}')
             user = sm.auth_user_oid(oidc.user_getfield('email'))
             log.debug(f'user: {user}')
             if user is None:
-                info = oidc.user_getinfo([
-                    USERNAME_OIDC_FIELD,
-                    FIRST_NAME_OIDC_FIELD,
-                    LAST_NAME_OIDC_FIELD,
-                    'email',
-                ])
+                # info = oidc.user_getinfo([
+                #     USERNAME_OIDC_FIELD,
+                #     FIRST_NAME_OIDC_FIELD,
+                #     LAST_NAME_OIDC_FIELD,
+                #     'email',
+                # ])
                 log.debug('sm.add_user')
+                email = oidc.user_getfield('email')
+                try:
+                    first_name = email.split('@')[0].split('.')[0]
+                    last_name = email.split('@')[0].split('.')[1]
+                except:
+                    first_name = 'FirstName'
+                    last_name = 'LastName'
                 user = sm.add_user(
-                    username=info.get(USERNAME_OIDC_FIELD),
-                    first_name=info.get(FIRST_NAME_OIDC_FIELD),
-                    last_name=info.get(LAST_NAME_OIDC_FIELD),
-                    email=info.get('email'),
+                    # username=info.get(USERNAME_OIDC_FIELD),
+                    # first_name=info.get(FIRST_NAME_OIDC_FIELD),
+                    # last_name=info.get(LAST_NAME_OIDC_FIELD),
+                    # email=info.get('email'),
+                    # role=sm.find_role(sm.auth_user_registration_role)
+
+                    username=email,
+                    first_name=first_name,
+                    last_name=last_name,
+                    email=email,
                     role=sm.find_role(sm.auth_user_registration_role)
+
                 )
 
             login_user(user, remember=False)
 
             try:
-                next_url = request.args["next"] or self.appbuilder.get_url_for_index
+                next_url = request.args["next"]
+                if next_url == '' or next_url is None:
+                    self.appbuilder.get_url_for_index
             except (KeyError, IndexError):
                 next_url = self.appbuilder.get_url_for_index
             log.debug(f'next_url {next_url}')
